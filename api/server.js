79f0ff3ce0e02742dfe472d5ec2618db0737677d
KROHNICK <business.kanekperez@gmail.com>
2019-02-28 14:28:33 -0800

    server auth routes, token middleware

--- api/server.js
+++ api/server.js
@@ -2,83 +2,18 @@ require("dotenv").config();
 
 const express = require("express");
 const server = express();
-const helmet = require("helmet");
-const bcrypt = require("bcryptjs");
-const cors = require("cors");
-const jwt = require("jsonwebtoken");
 
 const db = require("../data/db");
 const Users = require("../data/models/userModel");
 const configureMiddleware = require("./middleware");
-
-const secret = process.env.JWT_SECRET || "secret";
+const authRouter = require("../auth0/auth-router");
 
 configureMiddleware(server);
 
+server.use("/api/auth", authRouter);
+
 server.get("/api", (req, res) => {
   res.send("Server works.");
-});
-
-server.post("/api/register", (req, res) => {
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 12);
-  user.password = hash;
-  Users.add(user)
-    .then(saved => {
-      if (req.body.username || req.body.password || req.body.department) {
-        const token = generateToken(user);
-        res.status(201).json({
-          saved,
-          message: "Registered.",
-          token,
-          secret
-        });
-      } else {
-        res.status(500).json(err);
-      }
-    })
-    .catch(error => {
-      res.status(500).json({
-        message: "Unable to register."
-      });
-    });
-});
-
-function generateToken(user) {
-  const payload = {
-    subject: user.id,
-    username: user.username
-  };
-
-  const options = {
-    expiresIn: "1d"
-  };
-
-  return jwt.sign(payload, secret, options);
-}
-
-server.post("/api/login", (req, res) => {
-  let { username, password } = req.body;
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user);
-        res.status(200).json({
-          message: `Welcome ${
-            user.username
-          }! Successfully loggin in, here's a cookie and a token`,
-          token
-        });
-      } else {
-        res
-          .status(401)
-          .json({ message: "Invalid Credentials. You shall not pass!" });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
 });
 
 function restricted(req, res, next) {
---
+++ auth0/auth-router.js
@@ -0,0 +1,56 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+
+const tokenService = require("../auth0/token-service");
+const Users = require("../data/models/userModel");
+
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+  Users.add(user)
+    .then(saved => {
+      if (req.body.username || req.body.password || req.body.department) {
+        const token = generateToken(user);
+        res.status(201).json({
+          saved,
+          message: "Registered.",
+          token,
+          secret
+        });
+      } else {
+        res.status(500).json(err);
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Unable to register."
+      });
+    });
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenService.generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${
+            user.username
+          }! Successfully loggin in, here's a cookie and a token`,
+          token
+        });
+      } else {
+        res
+          .status(401)
+          .json({ message: "Invalid Credentials. You shall not pass!" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = router;
